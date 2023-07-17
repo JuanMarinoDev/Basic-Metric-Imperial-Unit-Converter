ba1e2c35f3ea496af313eed81ce6ad77aedad44c
juan <juangui097@gmail.com>
2023-07-17 01:30:07 -0500

    Created Convert function
    -Added meters-to-feet conversion.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-// _________________________________________________
-// -------------------  PROJECT  -------------------
-// -------------------------------------------------
+/*________________________________________________________________
+  ---------  Metric/Imperial Unit Converter JavaScript  ----------
+  ----------------------------------------------------------------*/
+const metersToFeet = document.querySelector('.meters');
+
+//p class="meters"
+const convert = function() {
+    let unit = parseFloat(document.querySelector(".unit-container").value);
+    let metersToFeetConversion = Math.floor(unit * 3.28084 * 1000) / 1000;
+    metersToFeet.innerHTML = `${unit} meters = ${metersToFeetConversion} feet`;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
