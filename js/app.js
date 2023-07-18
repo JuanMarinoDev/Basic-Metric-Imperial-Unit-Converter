44e07df7fba09fea7cd747534d69d6f82f633924
juan <juangui097@gmail.com>
2023-07-18 01:35:47 -0500

    Added Conversions
    -Added gallons-to-liters conversion.
    -Added kilos-to-pounds conversion.
    -Added pounds-to-kilos conversion.
    -Corrected value of Pounds to Kilos conversion.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,14 @@
 const metersToFeet = document.querySelector('.meters');
 const feetToMeters = document.querySelector('.feet');
 const litersToGallons = document.querySelector('.liters');
-//p class="meters"
+const gallonsToLiters = document.querySelector('.gallons');
+const kilosToPounds = document.querySelector('.kilograms');
+
+
+
+
+const poundsToKilos = document.querySelector('.pounds');
+
 const convert = function() {
     let unit = parseFloat(document.querySelector(".unit-container").value);
     // Conversion 1: Meters to Feet
@@ -16,7 +23,21 @@ const convert = function() {
     // Conversion 3: Liters to Gallons
     let litersToGallonsConversion = Math.floor(unit * 0.264172 * 1000) / 1000;
     litersToGallons.innerHTML = `${unit} liters = ${litersToGallonsConversion} gallons`;
-    // console.log(litersToGallons);
+    // Conversion 4: Gallons to Liters
+    let gallonsToLitersConversion = Math.floor(unit * 3.78541 * 1000) / 1000;
+    gallonsToLiters.innerHTML = `${unit} gallons = ${gallonsToLitersConversion} liters`;
+    // Conversion 5: Kilos to Pounds
+    let kilosToPoundsConversion = Math.floor(unit * 2.20462 * 1000) / 1000;
+    kilosToPounds.innerHTML = `${unit} kilos = ${kilosToPoundsConversion} pounds`;
+    // Conversion 6: Pounds to Kilos
+    let poundsToKilosConversion = Math.floor(unit * 0.453592 * 1000) / 1000;
+    poundsToKilos.innerHTML = `${unit} pounds = ${poundsToKilosConversion} kilos`;
+
+
+    
+
+
+
 };
 
 
